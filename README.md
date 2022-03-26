b5b2a63a45e288bb460aafeacb6ba7d475c1ceeb
Abinash Karmakar <darkrosesoftware@gmail.com>
2022-03-27 02:20:13 +0530

    UPDATE

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello I am Abinash
+## My [Website](https://codeAbinash.github.io/codeAbinash)"# codeAbinash" 
---
+++ js/index.js
@@ -0,0 +1,4 @@
+function openMenu(){
+    document.querySelector("header .d2").classList.toggle("on")
+    document.querySelector("header .d1 .menuBtn").classList.toggle("on")
+}
